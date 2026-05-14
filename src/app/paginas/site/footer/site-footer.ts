8caefc2f6431b5b00644a76db38668225d495e37
bruno <brunofss1985@gmail.com>
2026-05-14 10:17:03 -0300

    ajustes no css do site

--- src/app/paginas/site/footer/site-footer.ts
+++ src/app/paginas/site/footer/site-footer.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-site-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './site-footer.html',
   styleUrl: './site-footer.css'
 })
